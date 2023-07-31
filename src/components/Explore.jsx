ff3ce6bbe0bcb795c901b1b9041ab89d330a063a
qwertyNippon <orlandohenry100@gmail.com>
2023-07-31 17:59:02 -0400

    fixed cards and titles

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -13,6 +13,7 @@ function Explore() {
     }
     const load_Anime = async () => {
         const data = await get_Exp()
+        console.log(data)
         // .then(data => console.log(data))
         setAnime(data)
     }
@@ -25,14 +26,15 @@ function Explore() {
             {anime.posts && anime.posts.length > 0 ? Object.values(anime.posts).map((a, i) => {
                 return <div key={i} className="cardEx  flx-c mx">
                     <div className='cardB white flx-c p8 tog '>
-                        <div className="flx1">{a.rating}</div>
+                        <div className="flx1 lnk">{a.rating}</div>
                         <hr className="h" />
-                        <div className="flx1"><Link target='_blank' to={a.trail}>Watch Trailer</Link></div>
+                        <div className="flx1 lnk"><Link className="lnk" target='_blank' to={a.trail}>Watch Trailer</Link></div>
                         <hr className="h" />
-                        <div className="flx1"><Link target='_blank' to={a.new_url}>Anime Info</Link></div>
+                        <div className="flx1 lnk"><Link className="lnk" target='_blank' to={a.new_url}>Anime Info</Link></div>
                         <hr className="h" />
                         <div className="flx8 scrol">{a.syn}</div>
-                        <div className="flx1"><button>Add to Watch List</button></div>
+                        <div className="space"></div>
+                        <div className="flx1 lnk"><button className="addWL">Add to Watch List</button></div>
                     </div>
                     <div className="flx5  p8 "><img className="img" src={a.img} alt="" /></div>
                     <div className="flx1 title p8"><p className='title'>{a.engT}</p> </div>
